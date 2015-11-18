fca59b65c0a64d9cedca6ed8f8670c91cb8892dd
Alexander Korling <lermannen@gmail.com>
2015-11-18 20:18:52 +0100

    First solution for word_count.

---
+++ elixir/word-count/word_count.exs
@@ -0,0 +1,26 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map()
+  def count(sentence) do
+    do_work(prepare_string(sentence), Map.new)
+  end
+
+  defp do_work([], m), do: m
+
+  defp do_work([h|t], m) do
+    m = Map.put(m, h, Map.get(m, h, 0) + 1)
+    do_work(t, m)
+  end
+
+  defp prepare_string(sentence) do
+    String.split(String.downcase(replace_bad_chars(sentence)))
+  end
+
+  defp replace_bad_chars(sentence) do
+    String.replace(String.replace(sentence, ~r/[^\w -]/u, ""), "_", " ")
+  end
+end
