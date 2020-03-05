c51532cfcceb4eecc79fe94ba2956a8a58ef35ef
Alexander Korling <lermannen@gmail.com>
2020-03-05 08:55:11 +0100

    First implementation

---
+++ javascript/protein-translation/protein-translation.js
@@ -0,0 +1,44 @@
+export const translate = (input = "") => {
+    if (input == "") {
+	return [];
+    } else {
+	var s = input.match(/.{1,3}/g)
+	return do_it(s);
+    };
+};
+
+const do_it = (arr) => {
+    var results = arr.reduce((results, item) => {
+	results.push(trans(item));
+	return results;
+    }, []);
+    var res = [];
+    for(var e in results) {
+	if (results[e] == "STOP") { break; }
+	res.push(results[e]);
+    };
+    return res;
+};
+
+const trans = (input) => {
+    if (input == "AUG") {
+	return "Methionine";
+    } else if (input == "UUC" || input == "UUU") {
+	return "Phenylalanine";
+    } else if (input == "UUA" || input == "UUG") {
+	return "Leucine";
+    } else if (input == "UCU" || input == "UCC" || input == "UCA" || input == "UCG") {
+	return "Serine";
+    } else if (input == "UAU" || input == "UAC") {
+	return "Tyrosine";
+    } else if (input == "UGU" || input == "UGC") {
+	return "Cysteine";
+    } else if (input == "UGG") {
+	return "Tryptophan";
+    } else if (input == "UAA" || input == "UAG" || input == "UGA") {
+	return "STOP";
+    } else {
+	throw new Error("Invalid codon");
+    }
+};
+    
