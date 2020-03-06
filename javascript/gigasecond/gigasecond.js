bcf150b083d958253ac66f3b62f4c23748aa66f8
Alexander Korling <lermannen@gmail.com>
2020-03-06 08:03:22 +0100

    Constants shouldn't be redefined

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,4 +1,5 @@
+const GIGASECOND_IN_MS = Math.pow(10, 12);
+
 export const gigasecond = date => {
-    const gigasecond_in_ms = Math.pow(10, 12);
-    return new Date(date.getTime() + gigasecond_in_ms);
+    return new Date(date.getTime() + GIGASECOND_IN_MS);
 };
